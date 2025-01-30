dc683ff91a181845ea5ae308e5e990c5fe5b8d2c
Vladimir Elisavetski <yvova88@yahoo.com>
2025-01-30 12:23:52 -0700

    connecting SearcSvc to mongo

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,4 +13,14 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseAuthorization();
 app.MapControllers();
+
+await DB.InitAsync("SearchDb", MongoClientSettings
+    .FromConnectionString(builder.Configuration.GetConnectionString("MongoDBConnection")));
+
+await DB.Index<Item>()
+    .Key(x => x.Make, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Color, KeyType.Text)
+    .CreateAsync();
+
 app.Run();
