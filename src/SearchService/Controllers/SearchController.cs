d6fec2750a3b3b8532a2965a3d70f077d51224cb
Vladimir Elisavetski <yvova88@yahoo.com>
2025-01-30 12:53:12 -0700

    mongodb pagination

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+
+namespace SearchService;
+
+[ApiController]
+[Route("api/search")]
+public class SearchController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<Item>> SearchItems(
+        string searchTerm,
+        int pageNumber = 1,
+        int pageSize = 4
+    )
+    {
+
+        var query = DB.PagedSearch<Item>();
+
+        query.Sort(x => x.Ascending(a => a.Make));
+
+        if (!string.IsNullOrEmpty(searchTerm))
+        {
+            query.Match(Search.Full, searchTerm).SortByTextScore();
+        }
+
+        query.PageNumber(pageNumber);
+        query.PageSize(pageSize);
+
+        var result = await query.ExecuteAsync();
+
+        return Ok(
+            new
+            {
+                results = result.Results,
+                pageCount = result.PageCount,
+                totalCount = result.TotalCount,
+            });
+    }
+}
