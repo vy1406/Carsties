e23e3edf357ecb649abe360ce7be9017182ce1cd
Vladimir Elisavetski <yvova88@yahoo.com>
2025-02-02 11:37:16 -0700

    fixing internal network in identity server

--- src/IdentityService/HostingExtensions.cs
+++ src/IdentityService/HostingExtensions.cs
@@ -28,6 +28,11 @@ internal static class HostingExtensions
                 options.Events.RaiseFailureEvents = true;
                 options.Events.RaiseSuccessEvents = true;
 
+                if (builder.Environment.IsEnvironment("Docker"))
+                {
+                    options.IssuerUri = "http://localhost:5000";
+                    // for internal docker network
+                }
                 // see https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/
                 // options.EmitStaticAudienceClaim = true;
             })
