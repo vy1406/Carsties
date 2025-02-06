4ffbc41f17003f451df3659994bf5d0ffd4b3ade
Vladimir Elisavetski <yvova88@yahoo.com>
2025-02-05 17:41:26 -0700

    fixing eventBus error

--- src/AuctionService/Consumers/BidPlacedConsumer.cs
+++ src/AuctionService/Consumers/BidPlacedConsumer.cs
@@ -15,7 +15,7 @@ public class BidPlacedConsumer : IConsumer<BidPlaced>
     public async Task Consume(ConsumeContext<BidPlaced> context)
     {
         Console.WriteLine("BidPlacedConsumer: --> bid placed");
-        var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
+        var auction = await _dbContext.Auctions.FindAsync(Guid.Parse(context.Message.AuctionId));
 
         if (
             auction.CurrentHighBid == null
