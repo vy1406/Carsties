b8603c7833461806c9b0b58e34a17acb07622071
Vladimir Elisavetski <yvova88@yahoo.com>
2025-02-06 18:18:28 -0700

    notification service

---
+++ src/NotificationService/Hubs/NotificationHub.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace NotificationService
+{
+    public class NotificationHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
--- src/NotificationService/Program.cs
+++ src/NotificationService/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMassTransit(x =>
 {
-
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
     x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
 
     x.UsingRabbitMq((context, cfg) =>
