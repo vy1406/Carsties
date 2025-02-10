4bc203f9cf9ab57a465bf2844a3c96569983de19
Vladimir Elisavetski <yvova88@yahoo.com>
2025-02-10 12:36:22 -0700

    creating the first test

---
+++ tests/AuctionService.IntegrationTests/AuctionControllerTests.cs
@@ -0,0 +1,35 @@
+
+using System.Net.Http.Json;
+using AuctionService.Data;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuctionService;
+
+public class AuctionControllerTests : IClassFixture<CustomWebAppFactory>, IAsyncLifetime
+{
+    private readonly CustomWebAppFactory _factory;
+    private readonly HttpClient _httpClient;
+    public AuctionControllerTests(CustomWebAppFactory factory)
+    {
+        _factory = factory;
+        _httpClient = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task GetAuctions_ShouldReturn3Auctions()
+    {
+        var res = await _httpClient.GetFromJsonAsync<List<AuctionDto>>("/api/auctions");
+
+        Assert.Equal(3, res.Count);
+    }
+
+    public Task InitializeAsync() => Task.CompletedTask;
+    public Task DisposeAsync()
+    {
+        using var scope = _factory.Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<AuctionDbContext>();
+        DbHelper.ReinitDbForTests(db);
+        return Task.CompletedTask;
+    }
+
+}
